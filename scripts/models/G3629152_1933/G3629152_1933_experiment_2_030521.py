fc154e4f6e3f96bccd3c3d5ae93a86928c4363ae
shawnknabel <shawnknabel@gmail.com>
2021-03-05 14:32:05 -0800

    3rd model

---
+++ scripts/models/G3629152_1933/G3629152_1933_experiment_2_030521.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 3/5/21 - Second fit to G3629152_1933.
+
+# ## Experiment 2 : phase1_nlive = 200, phase1_tolerance = 0.5, phase2_nlive = 300, phase2_tolerance = 0.25, use effective radius from GAMA DR3 Sersic Photometry catalog (including errors) and upper limit (effective radius + error), normal mask, Source R_E (0->10*lens.inensity), 2 position, threshold=1.0
+# 
+# ## Don't take the instance for the ellipticity and effective radius. Set same max effective radius.
+
+# In[2]:
+
+
+### libraries
+#get_ipython().run_line_magic('matplotlib', 'inline')
+# set workspace path
+from pyprojroot import here
+workspace_path = str(here())#'/data/sknabel/autolens_workspace'
+#get_ipython().run_line_magic('cd', '$workspace_path')
+print(f"Working Directory has been set to `{workspace_path}`")
+
+import matplotlib.pyplot as plt
+from autoconf import conf
+import autolens as al
+import autolens.plot as aplt
+import autofit as af
+import pandas as pd
+import numpy as np
+from astropy.io import fits
+from astropy.visualization import astropy_mpl_style
+plt.style.use(astropy_mpl_style)
+from astropy.stats import sigma_clip as clip
+from os import path
+import time
+
+# set datetime variable
+datetime = time.strftime("%d%m%Y-%H%M%S")
+
+# paths
+autoz_path = '/data/sknabel/autoz_lens_model/'
+file_path = f'{autoz_path}files/'
+csv_path = f'{file_path}csv/'
+fits_path = f'{file_path}fits/'
+png_path = f'{autoz_path}visuals/png/'
+pdf_path = f'{autoz_path}visuals/pdf/'
+
+
+# In[3]:
+
+
+# this should be constant across experiments
+
+gama_id = 3629152
+links_id = 1933
+
+object_folder = f'{fits_path}G{gama_id}_{links_id}/'
+output_folder = f'{autoz_path}output/G{gama_id}_{links_id}/'
+
+# load object data table
+links = pd.read_csv(f'{csv_path}/latest/links_sample_latest.csv')
+lens_galaxy_data = links[links.GAMA_ID == gama_id]
+zlens=lens_galaxy_data.zlens.values
+zsource=lens_galaxy_data.zsource.values
+einstein_radius=np.mean([lens_galaxy_data.theta_e_pm.values, lens_galaxy_data.theta_e_sis.values]) # take average of einstein radius estimates for prior
+print(f'Lens and source redshifts at {zlens} and {zsource}.')
+print(f'Einstein radius prior: {einstein_radius}')
+
+# effective radius and error from DR3 sersic catalog
+re_r = 2.1932 # effect radius r
+re_r_err = 0.1860
+re_g = 1.9137
+re_g_err = 0.1999
+
+
+# load r-band imaging
+imaging_r = al.Imaging.from_fits(image_path=path.join(object_folder, f'{links_id}_r_image.fits'),
+                              noise_map_path=path.join(object_folder, f'{links_id}_r_noisemap.fits'),
+                              psf_path=path.join(object_folder, f'{links_id}_r_psf.fits'),
+                               pixel_scales=0.2)
+
+# load g-band imaging
+imaging_g = al.Imaging.from_fits(image_path=path.join(object_folder, f'{links_id}_g_image.fits'),
+                              noise_map_path=path.join(object_folder, f'{links_id}_g_noisemap.fits'),
+                              psf_path=path.join(object_folder, f'{links_id}_g_psf.fits'),
+                               pixel_scales=0.2)
+
+# set up mask
+mask = al.Mask2D.circular(
+    shape_native=imaging_r.shape_native, pixel_scales=imaging_r.pixel_scales, sub_size=2, radius=3.5
+)
+visuals_2d = aplt.Visuals2D(mask=mask)
+
+# plot subplots for first view
+print('Plotting r-band')
+imaging_plotter_r = aplt.ImagingPlotter( # this is where the noise is coming up weird
+    imaging=imaging_r, visuals_2d=aplt.Visuals2D(mask=mask)
+)
+imaging_plotter_r.subplot_imaging()
+print('Plotting g-band')
+imaging_plotter_g = aplt.ImagingPlotter( # this is where the noise is coming up weird
+    imaging=imaging_g, visuals_2d=aplt.Visuals2D(mask=mask)
+)
+imaging_plotter_g.subplot_imaging()
+
+# set up grid and settings
+settings_masked_imaging = al.SettingsMaskedImaging(grid_class=al.Grid2D)#, psf_shape_2d=imaging.psf.shape_2d)
+settings = al.SettingsPhaseImaging(settings_masked_imaging=settings_masked_imaging)
+
+#set up lens light profile
+lens_start = al.GalaxyModel(
+    redshift=zlens, bulge=al.lp.EllipticalSersic#, mass=al.mp.EllipticalIsothermal
+)
+
+# set priors
+# lens position
+lens_start.bulge.centre_0 = af.UniformPrior(lower_limit=-0.1, upper_limit=0.1)
+lens_start.bulge.centre_1 = af.UniformPrior(lower_limit=-0.1, upper_limit=0.1)
+# effective radius
+#lens_start.bulge.effective_radius = af.UniformPrior(lower_limit=0.0, upper_limit=5.0) # why have I chosen 3 here? because the mask is 3...
+
+
+# In[4]:
+
+
+# Start a pandas dataframe
+#performance_log = pd.DataFrame(columns=['Experiment', 
+#                                        'phase1_time', 
+#                                        'phase1_likelihood', 
+#                                        'phase2_time', 
+#                                        'phase2_likelihood' 
+#                                        ])
+
+# load performance log from csv
+performance_log = pd.read_csv(f'{csv_path}G{gama_id}_{links_id}_performance_log.csv')
+print(performance_log)
+#print(performance_log)
+
+
+# In[4]:
+
+
+# set experiment number
+experiment_number = '2'
+
+lens_start.bulge.effective_radius = af.GaussianPrior(mean=re_r, sigma=re_r_err, lower_limit=0.0, upper_limit=re_r+re_r_err)
+lens_start.bulge.centre_0 = af.UniformPrior(lower_limit=-0.4, upper_limit=0.0)
+lens_start.bulge.centre_1 = af.UniformPrior(lower_limit=0.0, upper_limit=0.4)
+
+# set up mask
+lens_mask = al.Mask2D.circular(
+    shape_native=imaging_r.shape_native, pixel_scales=imaging_r.pixel_scales, sub_size=2, radius=re_r, #centre=(0.075, -0.075) 
+)
+visuals_2d = aplt.Visuals2D(mask=lens_mask)
+
+# I can probably use the regular mask for this.
+
+# plot subplots for first view
+print('Plotting r-band')
+imaging_plotter_r = aplt.ImagingPlotter(
+    imaging=imaging_r, visuals_2d=visuals_2d
+)
+imaging_plotter_r.subplot_imaging()
+
+# set up phase
+phase1 = al.PhaseImaging(
+    search=af.DynestyStatic(
+        path_prefix=f"{output_folder}", name=f"experiment_{experiment_number}_phase1_{datetime}", n_live_points=200,
+        evidence_tolerance=0.5, walks = 10
+    ),
+    settings=settings,
+    galaxies=af.CollectionPriorModel(lens=lens_start)#, source=source)
+)
+
+print(lens_start)
+
+
+# In[10]:
+
+
+# run the phase
+print('Phase running...')
+tick = time.perf_counter()
+phase1_result = phase1.run(dataset=imaging_r, mask=mask)
+tock = time.perf_counter()
+print(f'Work complete! Took us {tock-tick} seconds or {(tock-tick)/60} minutes.')
+
+
+# In[11]:
+
+
+# write the results to the log
+print(f'Log likelihood: {phase1_result.log_likelihood}')
+print(f'Model: {phase1_result.model}')
+
+phase1_time = tock-tick
+
+# plot it!
+fit_imaging_plotter = aplt.FitImagingPlotter(fit=phase1_result.max_log_likelihood_fit)
+fit_imaging_plotter.subplot_fit_imaging()
+
+
+# In[22]:
+
+
+# now phase 2!
+
+#set up lens and source
+
+# set stellar mass/light profile
+bulge = af.PriorModel(al.lmp.EllipticalSersic)
+bulge.take_attributes(source=phase1_result.model.galaxies.lens.bulge)
+
+# set dark matter profile
+dark = af.PriorModel(al.mp.SphericalNFWMCRLudlow)
+dark.mass_at_200 = af.LogUniformPrior(lower_limit=1e8, upper_limit=1e15)
+dark.redshift_object = zlens
+dark.redshift_source = zsource
+
+lens = al.GalaxyModel(
+    redshift=zlens, bulge=bulge, dark=dark
+)
+
+source = al.GalaxyModel(
+    redshift=zsource, bulge=al.lp.SphericalExponential)
+
+# make lens intensity default to 0.1 +- 0.1 because red to green makes it wonky
+#lens.bulge.intensity = af.GaussianPrior(mean=0.1, sigma=0.1)
+
+# set uniform m_l ratio
+#lens.bulge.mass_to_light_ratio = af.UniformPrior(lower_limit=0.0, upper_limit=100000.0) # leave it default
+
+# make dark matter centered at stellar mass center
+lens.bulge.centre = phase1_result.instance.galaxies.lens.bulge.centre
+lens.dark.centre = lens.bulge.centre
+
+# make lens effective radius for g-band
+lens.bulge.effective_radius = af.GaussianPrior(mean=re_g, sigma=re_g_err, lower_limit=0.0, upper_limit=re_g+re_g_err)
+
+# einstein radius
+#lens.mass.einstein_radius = af.GaussianPrior(mean=einstein_radius, sigma=0.3*einstein_radius) # take sigma to be 30% of mean # hmmm
+
+# source position
+source.bulge.centre_0 = af.UniformPrior(lower_limit=-3, upper_limit=3)
+source.bulge.centre_1 = af.UniformPrior(lower_limit=-3, upper_limit=3)
+source.bulge.effective_radius = af.UniformPrior(lower_limit=0.0, upper_limit=5.0)
+#source.bulge.intensity = af.UniformPrior(lower_limit=0.0, upper_limit=10*lens.bulge.intensity)
+
+print(f'Lens: {lens}')
+print(f'Source: {source}')
+
+
+# In[9]:
+
+
+# Set up the positions... (GUI is not working...)
+
+# plot the r-band image to see it
+print('Plotting g-band')
+imaging_plotter_g = aplt.ImagingPlotter( # this is where the noise is coming up weird
+    imaging=imaging_g, visuals_2d=aplt.Visuals2D(mask=mask)
+)
+imaging_plotter_g.figures(image=True)
+
+# set positions
+imaging_g.positions = al.Grid2DIrregular(
+    [(-0.425, -2.1), (-0.0, -2.1)]#, (-0.8, 1.0), (-1.0, 0.5), (0.3, 1.1)]
+)
+
+# plot the image
+visuals_2d = aplt.Visuals2D(mask=mask, positions=imaging_g.positions)
+imaging_plotter_g = aplt.ImagingPlotter(imaging=imaging_g, visuals_2d=visuals_2d)
+imaging_plotter_g.figures(image=True)
+
+
+# In[39]:
+
+
+# set the settings to include the positions
+settings_lens = al.SettingsLens(positions_threshold=1.0)
+
+settings = al.SettingsPhaseImaging(
+    settings_masked_imaging=settings_masked_imaging, settings_lens=settings_lens
+)
+
+
+# In[40]:
+
+
+# set up phase
+phase2 = al.PhaseImaging(
+    search=af.DynestyStatic(
+        path_prefix=f'{output_folder}', name=f"experiment_{experiment_number}_phase2_fit_{datetime}", n_live_points=300,
+        evidence_tolerance=0.25, walks=10, facc=0.4
+    ),
+    settings=settings,
+    galaxies=af.CollectionPriorModel(lens=lens, source=source)#, source=source)
+)
+
+
+# In[42]:
+
+
+# run phase
+# run the phase
+print('Phase running...')
+tick = time.perf_counter()
+phase2_result = phase2.run(dataset=imaging_g, mask=mask)
+tock = time.perf_counter()
+print(f'Work complete! Took us {tock-tick} seconds or {(tock-tick)/60} minutes.')
+
+
+# In[43]:
+
+
+# write the results to the log
+print(f'Log likelihood: {phase2_result.log_likelihood}')
+print(f'Model: {phase2_result.model}')
+
+phase2_time=tock-tick
+
+log = open(f"{output_folder}experiment_log", 'a') # append the log
+lines = [f'Experiment {experiment_number} \n',
+         f'r-band \n',
+         f'Time to convergence: {phase1_time} seconds \n', 
+         f'Log likelihood: {phase1_result.log_likelihood} \n',
+         f'Model: {str(phase1_result.model)} \n',
+         f'g-band \n',
+         f'Time to convergence: {phase2_time} seconds \n', 
+         f'Log likelihood: {phase2_result.log_likelihood} \n', 
+         f'Model: {str(phase2_result.model)} \n', '\n'] # set lines to write the model result
+log.writelines(lines) # write lines
+log.close()
+
+
+# In[44]:
+
+
+# append performance log
+data_list = [[experiment_number, 
+            phase1_time,
+            phase1_result.log_likelihood,
+            phase2_time, 
+            phase2_result.log_likelihood,
+             ]]
+new_dataframe_entry = pd.DataFrame(data_list,
+                                  columns=['Experiment', 
+                                           'phase1_time', 
+                                           'phase1_likelihood', 
+                                           'phase2_time', 
+                                           'phase2_likelihood', 
+                                           ])
+performance_log = pd.concat([performance_log, new_dataframe_entry])
+print(performance_log)
+performance_log.to_csv(f'{csv_path}G{gama_id}_{links_id}_performance_log.csv')
+#print(phase_result.model)
+# get samples to see how it ran?
+#log_likelihoods = phase_result.samples.log_likelihoods
+#x = np.arange(0, len(log_likelihoods), 1)
+#plt.plot(x[500:], log_likelihoods[500:])
+#plt.xlim()
+
+# plot it!
+fit_imaging_plotter = aplt.FitImagingPlotter(fit=phase2_result.max_log_likelihood_fit)
+fit_imaging_plotter.subplot_fit_imaging()
+
+
+# In[ ]:
+
+
+aplt.show()
+
