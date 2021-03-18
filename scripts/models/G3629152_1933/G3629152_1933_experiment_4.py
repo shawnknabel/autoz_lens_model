c4353fbd47ad3daffe2fa692de02cfb688186f56
shawnknabel <shawnknabel@gmail.com>
2021-03-17 17:52:57 -0700

    best model 3212, simulations, and analysis

--- scripts/models/G3629152_1933/G3629152_1933_experiment_4.py
+++ scripts/models/G3629152_1933/G3629152_1933_experiment_4.py
@@ -212,7 +212,7 @@ phase1_time = tock-tick
 #set up lens and source
 
 # set stellar mass profile
-mass = al.mp.EllipticalSersic
+mass = al.mp.EllipticalSersic # This should have been EllipticalIsothermal
 #mass.take_attributes(source=phase1_result.model.galaxies.lens.bulge)
 
 lens = al.GalaxyModel(
